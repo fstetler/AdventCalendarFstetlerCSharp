797bf01a96b8e3d222a77a700d0d015f0e7cc893
frste <fredrik.stetler@knightec.se>
2024-01-25 11:42:49 +0100

    Made first test

--- dayone/DayOne.cs
+++ dayone/DayOne.cs
@@ -1,21 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventCalendarC_.dayone {
     public class DayOne : PrintSolution {
-        public int returnOne() {
-            return 1;
+        
+        public string? getFirstNumberFromString(string s, bool reversed, bool partOne) {
+
+            
+
+            List<string> numbersAsString = getNumbersAsString(s, reversed);
+            s = getString(s, reversed);
+
+            for (int i = 0; i < s.Length; i++ ) {
+                if (Char.IsDigit(s[i])) {
+                    return s[i].ToString();
+                } else {
+
+                }
+                for (int j = 0; j < numbersAsString.Count(); j++) {
+                    if (s.Substring(i).StartsWith(numbersAsString[j]) && !partOne) {
+                        return (j+1).ToString();
+                    }
+                }
+            }
+
+            return null;
         }
+
+        public List<string> getNumbersAsString(String s, Boolean reversed) { 
+            if (reversed) {
+                return ["eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"];
+            }
+            return ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        }
+
+        public string getString(string s, bool reversed) {
+            if (reversed) {
+                return reverseString(s);
+            }
+            return s;
+        }
+
+        private string reverseString(string s) {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+
+
+
 
         public void printSolutionOne() {
             Console.WriteLine(Util.getListOfStringsFromFile("C:\\Programming\\C#\\AdventCalendarC#\\resources\\dayone.txt")[0]);
         }
 
         public void printSolutionTwo() {
-            Console.WriteLine(returnOne());
+            Console.WriteLine();
         }
     }
 }
