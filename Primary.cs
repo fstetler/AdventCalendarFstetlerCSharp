d689a5cd7d318e7d973e509d73d632a872e22e81
frste <fredrik.stetler@knightec.se>
2024-01-25 14:12:53 +0100

    saved

--- Primary.cs
+++ Primary.cs
@@ -1,6 +1,7 @@
 ﻿using AdventCalendarC_.dayone;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,7 @@ namespace AdventCalendarCsharp {
     public class Primary {
         public static void Main(string[] args) {
             DayOne dayOne = new DayOne();
-            Console.WriteLine("TJENA");
             dayOne.printSolutionOne();
         }
-
     }
 }
--- dayone/DayOne.cs
+++ dayone/DayOne.cs
@@ -8,28 +8,26 @@ using System.Threading.Tasks;
 namespace AdventCalendarC_.dayone {
     public class DayOne : PrintSolution {
         
-        public string getFirstNumberFromString(string s, bool partOne) {
+        public int getFirstNumberFromString(string s, bool partOne) {
             List<string> numbersAsString = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
             return getNumberFromString(partOne, numbersAsString, s);
         }
 
-        public string getFirstNumberFromReversedString(string s, bool partOne) {
+        public int getFirstNumberFromReversedString(string s, bool partOne) {
             List<string> numbersAsString = ["eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"];
             String newS = reverseString(s);
             return getNumberFromString(partOne, numbersAsString, newS);
         }
 
-        private string getNumberFromString(bool partOne, List<string> numbersAsString, string newS) {
+        private int getNumberFromString(bool partOne, List<string> numbersAsString, string newS) {
             for (int i = 0; i < newS.Length; i++) {
                 if (Char.IsDigit(newS[i])) {
-                    Console.WriteLine("Hallå");
-                    return newS[i].ToString();
+                    return (int) Char.GetNumericValue(newS[i]);
                 }
                 if (!partOne) {
                     for (int j = 0; j < numbersAsString.Count(); j++) {
                         if (newS.Substring(i).StartsWith(numbersAsString[j])) {
-                            Console.WriteLine("hejsan");
-                            return (j + 1).ToString();
+                            return (j + 1);
                         }
                     }
                 }
@@ -38,6 +36,8 @@ namespace AdventCalendarC_.dayone {
             throw new Exception("hey");
         }
 
+
+
         private string reverseString(string s) {
             char[] charArray = s.ToCharArray();
             Array.Reverse(charArray);
@@ -45,7 +45,8 @@ namespace AdventCalendarC_.dayone {
         }
 
         public void printSolutionOne() {
-            Console.WriteLine(Util.getListOfStringsFromFile("C:\\Programming\\C#\\AdventCalendarC#\\resources\\dayone.txt")[0]);
+        
+
         }
 
         public void printSolutionTwo() {
