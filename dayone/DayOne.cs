d82b0712b7857ea5d0a49d6e01577c3b816bb4e7
frste <fredrik.stetler@knightec.se>
2024-01-25 15:25:48 +0100

    saved

--- dayone/DayOne.cs
+++ dayone/DayOne.cs
@@ -7,36 +7,64 @@ using System.Threading.Tasks;
 
 namespace AdventCalendarC_.dayone {
     public class DayOne : PrintSolution {
-        
-        public int getFirstNumberFromString(string s, bool partOne) {
-            List<string> numbersAsString = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-            return getNumberFromString(partOne, numbersAsString, s);
+
+        readonly List<string> reversedNumbersAsString = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+        readonly List<string> numbersAsString = ["eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"];
+
+
+        //public int solvePartOne(List<String> strings) {
+        //    for (int i = 0; i < strings.Count; i++) {
+        //        string s = strings[i];
+        //        int numberFromLeft = getFirstNumberFromStringPartOne(s);
+        //        int numberFromRight = getFirstNumberFromReversedStringPartOne(s);
+
+
+
+        //    }
+        //}
+
+        public List<int> numbersFromLeftPartOne(List<String> strings) {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < strings.Count; i++) {
+                int numberFromLeft = getFirstNumberFromStringPartOne(strings[i]);
+                numbers.Add(numberFromLeft);
+            }
+            return numbers;
         }
 
-        public int getFirstNumberFromReversedString(string s, bool partOne) {
-            List<string> numbersAsString = ["eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"];
+        public int getFirstNumberFromReversedStringPartOne(string s) {
             String newS = reverseString(s);
-            return getNumberFromString(partOne, numbersAsString, newS);
+            return getFirstNumberFromStringPartOne(newS);
         }
 
-        private int getNumberFromString(bool partOne, List<string> numbersAsString, string newS) {
-            for (int i = 0; i < newS.Length; i++) {
-                if (Char.IsDigit(newS[i])) {
-                    return (int) Char.GetNumericValue(newS[i]);
+        public int getFirstNumberFromStringPartOne(string s) {
+            for (int i = 0; i < s.Length; i++) {
+                if (Char.IsDigit(s[i])) {
+                    return (int) Char.GetNumericValue(s[i]);
                 }
-                if (!partOne) {
-                    for (int j = 0; j < numbersAsString.Count(); j++) {
-                        if (newS.Substring(i).StartsWith(numbersAsString[j])) {
-                            return (j + 1);
-                        }
-                    }
-                }
+             
             }
 //  gör ot till int
             throw new Exception("hey");
         }
 
-
+        public int getNumberFromStringPartTwo(bool partOne, List<string> numbersAsString, string s) {
+            for (int i = 0; i < s.Length; i++) {
+                if (Char.IsDigit(s[i])) {
+                    return (int)Char.GetNumericValue(s[i]);
+                }
+                if (!partOne) {
+                    for (int j = 0; j < numbersAsString.Count(); j++) {
+                        if (s.Substring(i).StartsWith(numbersAsString[j])) {
+                            return (j + 1);
+                        }
+                    }
+                }
+            }
+            throw new Exception("Shouldnt happen");
+        }
 
         private string reverseString(string s) {
             char[] charArray = s.ToCharArray();
@@ -50,7 +78,6 @@ namespace AdventCalendarC_.dayone {
         }
 
         public void printSolutionTwo() {
-            Console.WriteLine();
         }
     }
 }
