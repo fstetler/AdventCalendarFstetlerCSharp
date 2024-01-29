5195999656eae860bade4480c5cc23be8a2f03b8
fstetlerKnightec <152986163+fstetlerKnightec@users.noreply.github.com>
2024-01-29 11:50:42 +0100

    Day one part one (#2)
    * started dayonepartone
    
    * Made first test
    
    * saved
    
    * saved
    
    * saved
    
    * saved
    
    * saved
    
    * started over and made first test work
    
    * saved
    
    * made entire code work
    
    * saved
    
    * saved
    
    * saved
    
    * saved
    
    * finished
    
    * saved
    
    * moved all to correct dir
    
    * saved

--- AdventCalendarC#/daytwo/DayTwo.cs
+++ AdventCalendarC#/daytwo/DayTwo.cs
@@ -4,22 +4,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventCalendarC_.daytwo {
-    public class DayTwo {
 
-        public String getCutString(String currentGame) {
+namespace AdventCalendarC_.daytwo {
+    public class DayTwo : PrintSolution {
+
+        public string getCutString(string currentGame) {
             return string.Concat(" ", currentGame.AsSpan(currentGame.IndexOf(": ") + 2));
         }
 
-        public List<Round> getListOfRoundsFromCurrentGame(String currentGame, int gameIndex) {
+        public List<Round> getListOfRoundsFromCurrentGame(string currentGame, int gameIndex) {
+
+            string cutString = getCutString(currentGame);
+            string[] splitString = cutString.Split(";");
+            List<string> stringsForGames = new List<string>();
+            stringsForGames.AddRange(splitString);
+
+            List<Round> rounds = new List<Round>();
+
+            for (int i = 0; i < stringsForGames.Count; i++) {
+                string roundString = stringsForGames[i];
+                Round round = new Round(
+                    roundString,
+                    i,
+                    gameIndex,
+                    getNumberOfBall(roundString, "green"),
+                    getNumberOfBall(roundString, "red"),
+                    getNumberOfBall(roundString, "blue"));
+                rounds.Add(round);
+            }
+
+            return rounds;
+        }
+
+        public int sumOfAllValidIDs(List<string> gameStrings, int allowedGreen, int allowedBlue, int allowedRed) {
+            int totalSum = 0;
+            List<Game> gameObjects = getListOfAllGames(gameStrings);
+
+            for (int i = 0; i < gameObjects.Count; i++) {
+                if (gameObjects[i].canGameBePlayedWithFollowingBalls(allowedGreen, allowedBlue, allowedRed) {
+                    totalSum += gameObjects[i].GameIndex + 1;
+                }
+            }
+            return totalSum;
+        }
+
+        public int totalPowerOfAllGames(List<string> gameStrings) {
+            int totalPower = 0;
+
+            List<Game> gameObjects = getListOfAllGames(gameStrings);
+
+            for (int i = 0; i < gameObjects.Count; i++) {
+                totalPower += gameObjects[i].MaxNumberOfGreenNeeded * gameObjects[i].MaxNumberOfBlueNeeded * gameObjects[i].MaxNumberOfRedNeeded;
+            }
+            return totalPower;
+        }
+
+        public List<Game> getListOfAllGames(List<string> gamesAsStrings) {
+            List<Game> gamesAsObjects = new List<Game>();
+
+            for (int i = 0; i < gamesAsStrings.Count(); i++) {
+                gamesAsObjects.Add(getGameObjectFromGameString(gamesAsStrings[i], i);
+            }
+            return gamesAsObjects;
+        }
+
+        public Game getGameObjectFromGameString(string currentGameAsString, int index) {
+            List<Round> rounds = getListOfRoundsFromCurrentGame(currentGameAsString, index);
+            Game game = new Game(index);
+
+            for (int i = 0; i < rounds.Count;i++) {
+                game.maxNumberOfBall(rounds[i], "green");
+                game.maxNumberOfBall(rounds[i], "blue");
+                game.maxNumberOfBall(rounds[i], "red");
+            }
+
+            return game;
 
             String 
         }
 
+        public static int getNumberOfBall(string currentRound, string color) { 
+            for (int i = 0; i < currentRound.Length; i++) {
+                if (currentRound.Substring(i).StartsWith(color) {
+                    return 10 * 
+                }
+            }
 
+        }
 
+        public static int convertCharacterAtIndexToInt(string currentGame, int index, int offset) {
+            return int.Parse(currentGame[index - offset].ToString());
+        }
 
+        public int results(bool isPartOne) {
+            List<string> games = Util.getListOfStringsFromFile("C:\\Programming\\C#\\AdventCalendarC#\\AdventCalendarC#\\resources\\daytwo.txt");
+            if (isPartOne) {
+                return sumOfAll
+            }
+        }
 
+        public void printSolutionOne() {
+            throw new NotImplementedException();
+        }
 
+        public void printSolutionTwo() {
+            throw new NotImplementedException();
+        }
     }
 }
