41b31c6695107373b04031418005489302973e24
frste <fredrik.stetler@knightec.se>
2024-01-31 09:02:39 +0100

    saved

--- AdventCalendarC#/daytwo/DayTwo.cs
+++ AdventCalendarC#/daytwo/DayTwo.cs
@@ -10,50 +10,46 @@
             Console.WriteLine("Part two = " + ResultsPartTwo());
         }
 
-        public string GetCutString(string currentGame) {
-            return string.Concat(" ", currentGame.AsSpan(currentGame.IndexOf(": ") + 2));
-        }
-
-        public List<Round> GetListOfRoundsFromCurrentGame(string currentGame, int gameIndex) {
-
-            string cutString = GetCutString(currentGame);
-            string[] splitString = cutString.Split(";");
-            List<string> stringRoundsForGames = [.. splitString];
-
-            List<Round> rounds = [];
-
-            return stringRoundsForGames.Select(r => 
-                new Round(GetNumberOfBall(r, "green"), GetNumberOfBall(r, "red"), GetNumberOfBall(r, "blue"))).ToList();
-        }
-
-        public int SumOfAllValidIDs(List<string> gameStrings, int allowedGreen, int allowedBlue, int allowedRed) {
-            List<Game> gameObjects = GetListOfAllGames(gameStrings);
+        public int SumOfAllValidIDs(List<Game> gameObjects, int allowedGreen, int allowedBlue, int allowedRed) {
             return gameObjects.Where(g => g.CanGameBePlayedWithFollowingBalls(allowedGreen, allowedBlue, allowedRed)).Select(g => g.GameIndex+1).Sum();
         }
 
-        public int TotalPowerOfAllGames(List<string> gameStrings) {
-            List<Game> gameObjects = GetListOfAllGames(gameStrings);
+        public int TotalPowerOfAllGames(List<Game> gameObjects) {
             return gameObjects.Select(g => g.MaxNumberOfGreenNeeded * g.MaxNumberOfBlueNeeded * g.MaxNumberOfRedNeeded).Sum();
         }
 
         public List<Game> GetListOfAllGames(List<string> gamesAsStrings) {
-            List<Game> gamesAsObjects = [];
+            return gamesAsStrings.Select(gs => GetGameObjectFromGameString(gs, gamesAsStrings.IndexOf(gs))).ToList();
+        }
 
-            for (int i = 0; i < gamesAsStrings.Count(); i++) {
-                gamesAsObjects.Add(GetGameObjectFromGameString(gamesAsStrings[i], i));
-            }
-            return gamesAsObjects;
+        public List<string> getRoundsAsStringsForGames(string cutString) {
+            string[] splitString = cutString.Split(";");
+            List<string> stringRoundsForGames = [.. splitString];
+            return stringRoundsForGames;
+        }
+
+        public List<Round> GetListOfRoundsFromCurrentGame(string currentGame) {
+
+            string cutString = GetCutString(currentGame);
+            List<string> stringRoundsForGames = getRoundsAsStringsForGames(cutString);
+
+            return stringRoundsForGames.Select(r =>
+                new Round(
+                    GetNumberOfBall(r, "green"),
+                    GetNumberOfBall(r, "red"),
+                    GetNumberOfBall(r, "blue")))
+                .ToList();
         }
 
         public Game GetGameObjectFromGameString(string currentGameAsString, int index) {
-            List<Round> rounds = GetListOfRoundsFromCurrentGame(currentGameAsString, index);
+            List<Round> rounds = GetListOfRoundsFromCurrentGame(currentGameAsString);
             Game game = new(index);
 
-            for (int i = 0; i < rounds.Count; i++) {
-                game.MaxNumberOfBall(rounds[i], "green");
-                game.MaxNumberOfBall(rounds[i], "blue");
-                game.MaxNumberOfBall(rounds[i], "red");
-            }
+            rounds.ForEach(r => {
+                game.MaxNumberOfBall(r, "green");
+                game.MaxNumberOfBall(r, "blue");
+                game.MaxNumberOfBall(r, "red");
+            });
 
             return game;
         }
@@ -61,35 +57,46 @@
         public int GetNumberOfBall(string currentRound, string color) {
             for (int i = 0; i < currentRound.Length; i++) {
                 if (currentRound.Substring(i).StartsWith(color)) {
-                    if (CharacterAtIndexNotBlank(currentRound, i)) {
-                        return 10 * ConvertCharacterAtIndexToInt(currentRound, i, 3) + ConvertCharacterAtIndexToInt(currentRound, i, 2);
-                    } else {
-                        return ConvertCharacterAtIndexToInt(currentRound, i, 2);
-                    }
+                    return TotalNumberOfBallsForOneColor(currentRound, i);         
                 }
             }
             return 0;
-
         }
 
-        private bool CharacterAtIndexNotBlank(string currentGame, int index) {
+        public int TotalNumberOfBallsForOneColor(string currentRound, int index) {
+            if (BallColorHasValueOver10(currentRound, index)) {
+                return 10 * ConvertCharacterAtIndexToInt(currentRound, index, 3) + ConvertCharacterAtIndexToInt(currentRound, index, 2);
+            } else {
+                return ConvertCharacterAtIndexToInt(currentRound, index, 2);
+            }
+        }
+
+        public bool BallColorHasValueOver10(string currentGame, int index) {
             return !char.IsWhiteSpace(currentGame[index - 3]);
         }
 
-        private int ConvertCharacterAtIndexToInt(string currentGame, int index, int offset) {
+        public int ConvertCharacterAtIndexToInt(string currentGame, int index, int offset) {
             return int.Parse(currentGame[index - offset].ToString());
         }
 
-        private List<string> InputStrings() {
+        public List<string> InputStrings() {
             return Util.getListOfStringsFromFile("resources\\daytwo.txt");
         }
 
-        private int ResultPartOne() {
-            return SumOfAllValidIDs(InputStrings(), 13, 14, 12);
+        public int ResultPartOne() {
+            List<string> inputStrings = InputStrings();
+            List<Game> gameObjects = GetListOfAllGames(inputStrings);
+            return SumOfAllValidIDs(gameObjects, 13, 14, 12);
         }
 
-        private int ResultsPartTwo() {
-            return TotalPowerOfAllGames(InputStrings());
+        public int ResultsPartTwo() {
+            List<string> inputStrings = InputStrings();
+            List<Game> gameObjects = GetListOfAllGames(inputStrings);
+            return TotalPowerOfAllGames(gameObjects);
+        }
+
+        public string GetCutString(string currentGame) {
+            return string.Concat(" ", currentGame.AsSpan(currentGame.IndexOf(": ") + 2));
         }
     }
 }
