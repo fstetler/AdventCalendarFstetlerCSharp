5b7666f41b7326910f33f208f6468cf1d9de2a72
frste <fredrik.stetler@knightec.se>
2024-02-02 09:37:38 +0100

    started day4

---
+++ AdventCalendarC#/dayfour/DayFour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCalendarC_.dayfour {
+    public class DayFour : PrintSolution {
+
+        public List<string>? removeFrontPartOfStrings(List<string> strings) {
+            return strings.Select(s => s.Split(":").Last()).ToList();
+        }
+
+        public void PrintSolutionOne() {
+            throw new NotImplementedException();
+        }
+
+        public void PrintSolutionTwo() {
+            throw new NotImplementedException();
+        }
+
+        public List<string> getStringsFromFile() {
+            return Util.getListOfStringsFromFile("resources\\dayfour.txt");
+        }
+
+    }
+}
---
+++ TestAdventCalendarC#/TestDayFour.cs
@@ -0,0 +1,23 @@
+﻿using AdventCalendarC_.dayfour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAdventCalendarC_ {
+    public class TestDayFour {
+
+        [Test]
+        public void CanReadLine() {
+            string line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+            List<string> strings = new List<string>();
+            strings.Add(line);
+
+            DayFour dayFour = new DayFour();
+            List<string> cutStrings = dayFour.removeFrontPartOfStrings(strings);
+            Console.WriteLine(cutStrings[0]);
+        }
+
+    }
+}
